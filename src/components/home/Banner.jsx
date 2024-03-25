ae4de2b7089dbc929fb338ba3bd14c68b1501103
Yash <yashsolanki0728@gmail.com>
2024-03-25 15:32:45 +0530

    Banner added

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { bannerData } from "../../constants/data";
+
+const responsive = {
+  // superLargeDesktop: {
+  //   breakpoint: { max: 4000, min: 3000 },
+  //   items: 5,
+  // },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <Carousel
+      responsive={responsive}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+      containerClass="carousel-container"
+      swipeable={false}
+      draggable={false}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={4000}
+      keyBoardControl={true}
+      slidesToSlide={1}
+    >
+      {bannerData.map((data) => (
+        <img
+          src={data.url}
+          alt="banner"
+          style={{ width: "100%", height: "280px" }}
+        />
+      ))}
+    </Carousel>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Banner from "./Banner";
+import { Box, styled } from "@mui/material";
+
+const Component = styled(Box)`
+  padding: 10px;
+  background: #f2f2f2;
+`;
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Banner />
+      <Component>
+        <Banner />
+      </Component>
     </>
   );
 };
